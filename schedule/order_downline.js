66e4743ced358ccd3b11113f6192ecd7919ac5e2
宋连盟 <songlianmeng@dding.cn>
2019-04-03 17:20:58 +0800

    fix：schedule

--- schedule/order_downline.js
+++ schedule/order_downline.js
@@ -4,10 +4,7 @@ const redis = require('../lib/redis');
 const moment = require('moment');
 const common = require('../lib/common');
 // 每天0点执行
-const j = schedule.scheduleJob('1 8 * * * *', async function() {
-  console.log('schedule start')
-  const lock = await common.redlock.lock(`scheduleLock`, 300000);
-  console.log('schedule doing')
+const j = schedule.scheduleJob('1 0 0 * * *', async function() {
 
   const yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
   // 渠道活跃用户数统计
@@ -52,8 +49,5 @@ const j = schedule.scheduleJob('1 8 * * * *', async function() {
   }
   // 自动下线
   await db.collection('order').update({ endDate: { $lte: new Date()}}, { $set: { online: 0 } });
-
-  await app.redlock.unlock(lock);
-  console.log('schedule end')
 
 });
